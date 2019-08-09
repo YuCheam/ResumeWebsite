f35b8f37f8074dbe687f2f010ea46dafd84c0e20
Kha-Yu Cheam <kac6294@arl.psu.edu>
2019-08-09 15:50:24 -0400

    fine tuning

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let isScrolling;
 let scrollDisabled = false;
+let activeHeight = 0;
+let coverImage = document.getElementById('coverImage');
 
 window.addEventListener('wheel', function(e) {
     if (!scrollDisabled) {
@@ -15,15 +17,18 @@ function snapEffect(event) {
     scrollDisabled = true;
     setTimeout(function() {
         scrollDisabled = false;
-    }, 600);
+        coverImage.classList.remove('active');
+    }, 700);
 
     const container = document.getElementById('container');
     const screenHeight = window.innerHeight;
 
     if (event.deltaY < 0 ) {
-        container.style.transform = `translate3d(0, ${container.getBoundingClientRect().y + screenHeight}px, 0)`
+        activeHeight += screenHeight;
+        container.style.transform = `translate3d(0, ${activeHeight}px, 0)`
     } else {
-        container.style.transform = `translate3d(0, ${container.getBoundingClientRect().y - screenHeight}px, 0)`
+        activeHeight -= screenHeight;
+        container.style.transform = `translate3d(0, ${activeHeight}px, 0)`
     }
 
 }
