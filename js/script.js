1d7691de033b858dc1614756bfd521b334fb1e14
Kha-Yu Cheam <kac6294@arl.psu.edu>
2019-08-09 10:28:56 -0400

    Parallax with javascript, Edge support okay

--- js/script.js
+++ js/script.js
@@ -1,26 +1,10 @@
-const container = document.getElementById('container');
-let offsetTop = document.getElementById('nav').offsetTop;
+const parallax  = document.getElementById('parallax');
 
-container.addEventListener('scroll', function() {
-    stickHeader();
+window.addEventListener('scroll', function() {
+    window.requestAnimationFrame(parallaxEffect)
 });
 
+function parallaxEffect() {
+    parallax.style.transform = `translateY(${1+(window.pageYOffset*.5)}px)`
 
-/*function parallaxScrolling() {
-    let bkg1 = document.getElementById('bkg1');
-    window.requestAnimationFrame(function() {
-        bkg1.style.transform = 'translate(0, ' + window.pageYOffset * .75 + 'px)';
-    });
-}*/
-function stickHeader() {
-    let nav = document.getElementById('nav');
-
-    console.log(container.scrollTop, offsetTop)
-    if(container.scrollTop > offsetTop) {
-        nav.classList.add('sticky');
-        nav.style.top = container.scrollTop + 'px';
-    } else {
-        nav.classList.remove('sticky');
-        nav.style.top = '0';
-    }
 }
