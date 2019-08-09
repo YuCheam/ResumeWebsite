1e5d19ab08220ec4eab42b526ace6b14bdc0fb99
Kha-Yu Cheam <kac6294@arl.psu.edu>
2019-08-09 09:24:25 -0400

    Parallax Scrolling with pure CSS, does not work on edge

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-window.addEventListener('scroll', function() {
-    let scrollVal = this.scrollY;
-    let main = document.getElementById('main');
-    let bkg1 = document.getElementById('bkg1');
+const container = document.getElementById('container');
+let offsetTop = document.getElementById('nav').offsetTop;
 
-    window.requestAnimationFrame(function() {
-        bkg1.style.transform = `translate(0, ${scrollVal * .75}px)`
-    });
-
-    console.log(scrollVal, main.offsetTop);
-    if(scrollVal > main.offsetTop) {
-        document.getElementById('nav').classList.add('sticky')
-    } else {
-        document.getElementById('nav').classList.remove('sticky')
-    }
+container.addEventListener('scroll', function() {
+    stickHeader();
 });
+
+
+/*function parallaxScrolling() {
+    let bkg1 = document.getElementById('bkg1');
+    window.requestAnimationFrame(function() {
+        bkg1.style.transform = 'translate(0, ' + window.pageYOffset * .75 + 'px)';
+    });
+}*/
+function stickHeader() {
+    let nav = document.getElementById('nav');
+
+    console.log(container.scrollTop, offsetTop)
+    if(container.scrollTop > offsetTop) {
+        nav.classList.add('sticky');
+        nav.style.top = container.scrollTop + 'px';
+    } else {
+        nav.classList.remove('sticky');
+        nav.style.top = '0';
+    }
+}
