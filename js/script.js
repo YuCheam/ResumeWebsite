119076fb8ee5a34116c7634d390327158a9dbafe
Kha-Yu Cheam <kac6294@arl.psu.edu>
2019-08-09 11:13:13 -0400

    Sticky Header added, parallax still working in Edge with addition

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 const parallax  = document.getElementById('parallax');
 
 window.addEventListener('scroll', function() {
-    window.requestAnimationFrame(parallaxEffect)
+    window.requestAnimationFrame(parallaxEffect);
+    stickyHeader();
 });
 
 function parallaxEffect() {
-    parallax.style.transform = `translateY(${1+(window.pageYOffset*.5)}px)`
+    parallax.style.transform = `translateY(${1+(window.pageYOffset*.3)}px)`
+}
 
+let offsetTop = document.getElementById('nav').offsetTop;
+
+function stickyHeader() {
+    if (window.pageYOffset >= offsetTop) {
+        document.getElementById('nav').classList.add('sticky')
+    } else {
+        document.getElementById('nav').classList.remove('sticky')
+    }
 }
