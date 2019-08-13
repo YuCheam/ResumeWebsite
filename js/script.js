1c99a82d1157fe0fb8e8b68ed4cfe9c482f777cf
Kha-Yu Cheam <kac6294@arl.psu.edu>
2019-08-13 15:53:05 -0400

    Animations in progress for aboutme page

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ let animatedElem = document.querySelectorAll('.animate');
 let elementTop = Array.from(animatedElem, x => x.getBoundingClientRect().top);
 const container = document.getElementById('container');
 
-
 window.addEventListener('wheel', function(e) {
     if (!scrollDisabled) {
         clearTimeout(isScrolling);
@@ -18,18 +17,22 @@ window.addEventListener('wheel', function(e) {
 
 function snapEffect(event) {
     scrollDisabled = true;
-    setTimeout(function() {
+    setTimeout(function () {
         scrollDisabled = false;
     }, 700);
 
-    if (event.deltaY < 0 ) {
+    if (event.deltaY < 0) {
         activeHeight += 100;
-        elementTop = elementTop.map(item => {return item += window.innerHeight});
+        elementTop = elementTop.map(item => {
+            return item += window.innerHeight
+        });
         console.log('up')
         container.style.transform = `translate3d(0, ${activeHeight}vh, 0)`
     } else {
         activeHeight -= 100;
-        elementTop = elementTop.map((item => {return item -= window.innerHeight}));
+        elementTop = elementTop.map((item => {
+            return item -= window.innerHeight
+        }));
         container.style.transform = `translate3d(0, ${activeHeight}vh, 0)`;
     }
 
@@ -37,9 +40,9 @@ function snapEffect(event) {
 }
 
 function checkPosition() {
-    for (x of elementTop) {
-        let element = animatedElem[elementTop.indexOf(x)];
-        if( x < window.innerHeight && x >= 0 ) {
+    for (let i = 0; i < elementTop.length; i++) {
+        let element = animatedElem[i];
+        if( elementTop[i] < window.innerHeight && elementTop[i] >= 0 ) {
             element.className = element.className.replace('animate', 'active');
         } else {
             element.className = element.className.replace('active', 'animate');
