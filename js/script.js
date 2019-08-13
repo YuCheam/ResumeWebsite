b8a8218beba6c77906b8563dfbda5a023e618751
Kha-Yu Cheam <kac6294@arl.psu.edu>
2019-08-13 10:00:04 -0400

    Added animation on window active for name and subname

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let isScrolling;
 let scrollDisabled = false;
 let activeHeight = 0;
-let coverImage = document.getElementById('coverImage');
+let hiddenElem = document.querySelectorAll('.hidden');
+const container = document.getElementById('container');
+
 
 window.addEventListener('wheel', function(e) {
     if (!scrollDisabled) {
@@ -17,18 +19,52 @@ function snapEffect(event) {
     scrollDisabled = true;
     setTimeout(function() {
         scrollDisabled = false;
-        coverImage.classList.remove('active');
     }, 700);
 
-    const container = document.getElementById('container');
     const screenHeight = window.innerHeight;
 
     if (event.deltaY < 0 ) {
         activeHeight += screenHeight;
+        console.log('up');
         container.style.transform = `translate3d(0, ${activeHeight}px, 0)`
     } else {
         activeHeight -= screenHeight;
         container.style.transform = `translate3d(0, ${activeHeight}px, 0)`
     }
 
+    setTimeout(checkPosition, 700)
 }
+
+function checkPosition() {
+    let rangeBottom = container.getBoundingClientRect().top +activeHeight;
+    let rangeTop = rangeBottom + window.innerHeight;
+    console.log(rangeBottom, rangeTop)
+    for (x of hiddenElem) {
+        let offset = x.getBoundingClientRect().top;
+        if( offset <= rangeTop && offset >= rangeBottom ) {
+            switch ( x.id ) {
+                case 'subName':
+                    x.className = x.className.replace('hidden', 'subName-fade-in');
+                    break;
+                case 'name':
+                    x.className = x.className.replace('hidden', 'name-fade-in');
+                    break;
+            }
+        } else {
+            switch (x.id) {
+                case 'subName':
+                    x.className = x.className.replace('subName-fade-in', 'hidden');
+                    break;
+                case 'name':
+                    x.className = x.className.replace('name-fade-in', 'hidden');
+            }
+        }
+    }
+}
+
+function initialize() {
+    checkPosition()
+}
+
+
+initialize();
